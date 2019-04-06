963acda87a462633ada3f46a93235a24fe18a6c7
Hieu <hieu@thanhhieu>
2019-04-06 19:21:13 +0700

    first

---
+++ LabBksce/lab4/ex1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+/**
+Print out the array 10x10 with option.
+*/
+void printArray(int option, int arr[10][10]){
+
+
+
+
+}
