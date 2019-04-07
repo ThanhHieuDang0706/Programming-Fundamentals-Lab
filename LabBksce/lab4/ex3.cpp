82caf8a7e878c01d246ffa62ade2931202902d10
Hieu <hieu@thanhhieu>
2019-04-07 12:17:58 +0700

    Only 7 passed

--- LabBksce/lab4/ex3.cpp
+++ LabBksce/lab4/ex3.cpp
@@ -6,7 +6,11 @@
 using namespace std;
 
 /* Write the code to calculate and print to screen */
-void multipleMatrix(int mA, int nA, int** A, int mB, int nB, int** B) {
+void multipleMatrix(int mA, int nA, int A[2][2], int mB, int nB, int B[2][2]) {
+    if (nA != mB) {
+        cout << "-1";
+        return;
+    }
     int mutiply_matrix[mA][nB];
     for (int i = 0; i < mA; ++i) {          
         for (int j = 0; j < nB; ++j) {
@@ -19,9 +23,22 @@ void multipleMatrix(int mA, int nA, int** A, int mB, int nB, int** B) {
     for (int i = 0; i < mA; ++i) {
         for (int j = 0; j < nB; ++j) {
             cout << mutiply_matrix[i][j];
-            if (j != 0) cout << ' ';
+            if (j != (nB - 1)) cout << ' ';
         }
         cout << endl;
     }
     
+}
+
+int main () {
+    int a[2][2] = {
+        {2, 1},
+        {3, 4}
+    };
+    int b[2][2] = {
+        {3,1},
+        {1, 1}
+    };
+    multipleMatrix(2, 2, a, 2, 2, b);
+    return 0;
 }
