4146a573795eac9655b45e908a7ba068ecfbf0cb
Hieu <hieu@thanhhieu>
2019-04-13 10:45:09 +0700

    test

--- Assignment_2/assignment1b/musketeer.cpp
+++ Assignment_2/assignment1b/musketeer.cpp
@@ -12,6 +12,7 @@
 #include <fstream>
 #include <string>
 #include <math.h>
+#include <iomanip>
 using namespace std;
 
 void combat(int hp1, int hp2, int d) {
@@ -25,8 +26,8 @@ void combat(int hp1, int hp2, int d) {
     p = ((double)h1 * p1 - (double)h2 * p2) / ((double)h1 * p1 + (double)h2 * p2);
 
     //process p as described if p is negative or greater than 1
-    if (p < 0.00l) p = 0.00l;
-    if (p > 1.00l) p = 1.00l;
+    // if (p < 0.00l) p = 0.00l;
+    // if (p > 1.00l) p = 1.00l;
 
     // Mau = 0
     if (((double)h1 * p1 + (double)h2 * p2) == 0.00l) {
@@ -92,34 +93,31 @@ void combat(int hp1, int hp2, int d) {
     }
 
     /* process the output */
-
-    cout.precision(2); 
-    cout.setf(ios::fixed, ios::floatfield);
-    cout << p;
+    cout << fixed << setprecision(2) << p;
 
 
 }
 
 int readFile(string filename, int& hp1, int& hp2, int& d) {
-    ifstream myfile(filename);
-    if (myfile.is_open()) {
-        myfile >> hp1 >> hp2 >> d;
+    // ifstream myfile(filename);
+    // if (myfile.is_open()) {
+    //     myfile >> hp1 >> hp2 >> d;
 
-        return 1;
-    }
-    else return 0;
+    //     return 1;
+    // }
+    // else return 0;
 }
 
 int main(int argc, char** argv) {
-    if (argc == 2) {
-        int hp1 = 0;
-        int hp2 = 0;
-        int d = 0;
+    // if (argc == 2) {
+    //     int hp1 = 0;
+    //     int hp2 = 0;
+    //     int d = 0;
 
-        int readInput = readFile(argv[1], hp1, hp2, d);
+    //     int readInput = readFile(argv[1], hp1, hp2, d);
         
-        if (readInput) combat(hp1, hp2, d);
-        else cout << "Cannot read input file";
-    }
-    else cout << "Incorrect arguments format";
+    //     if (readInput) combat(hp1, hp2, d);
+    //     else cout << "Cannot read input file";
+    // }
+    // else cout << "Incorrect arguments format";
 }
