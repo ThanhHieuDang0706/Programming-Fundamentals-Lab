6dd5d4e7f29d8c69d8779abbaf3915f911f42c5d
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2019-04-13 11:18:11 +0700

    Add files via upload

--- Assignment_2/assignment1b/musketeer.cpp
+++ Assignment_2/assignment1b/musketeer.cpp
@@ -12,6 +12,7 @@
 #include <fstream>
 #include <string>
 #include <math.h>
+#include <iomanip>
 using namespace std;
 
 void combat(int hp1, int hp2, int d) {
@@ -25,8 +26,8 @@ void combat(int hp1, int hp2, int d) {
     p = ((double)h1 * p1 - (double)h2 * p2) / ((double)h1 * p1 + (double)h2 * p2);
 
     //process p as described if p is negative or greater than 1
-    if (p < 0.00l) p = 0.00l;
-    if (p > 1.00l) p = 1.00l;
+    if (p <= 0.00l) p = 0.00l;
+    else if (p >= 1.00l) p = 1.00l;
 
     // Mau = 0
     if (((double)h1 * p1 + (double)h2 * p2) == 0.00l) {
@@ -35,86 +36,107 @@ void combat(int hp1, int hp2, int d) {
         else p = 1.00l;
     }   
 
-        /* Since if hp2 = 888 and hp1 = special numbers
-        all of the ablilities of hp1 will be useless 
-        so we will consider 2 conditions of hp2 here
-        Attention that in each condition of hp2 I use switch case statements
-        because writing with if else statement is super complicated
-        especially with the case of hp == 882 */
-        
-    if (hp2 != 888) {
-        switch (hp1) {
-            case 777: {
-                    if (p1 < p2 || h1 < h2) {
-                    d = 1;
-                    p1 = (double)hp1 * (double)(1000.00l - d) / (double)1000.00;
-                    p2 = (double)hp2 * (double)d / (double)1000.00;
-                    p = ((double)h1 * p1 - (double)h2 * p2) / ((double)h1 * p1 + (double)h2 * p2);
-                }
-                    break;
-            }
-        
-            case 888 : { 
-                h1 *= 10; // Don't change h2 accordingly
-                p = ((double)h1 * p1 - (double)h2 * p2) / ((double)h1 * p1 + (double)h2 * p2);
-                break;
-            }
-    
-            case 900 : {
-                if  (p < 0.50l) p = 0.50l;
-                break;    
-            }   
-        }
-
-        if (d != 987) { // Since with d = 987 and with special pairs of hp1 and hp2 below, it will end up a different result
-            if ((hp1 == 220 && hp2 == 284) || (hp1 == 284 && hp2 == 220)) p = 0.50l; 
-        }
-        else { // in the case of d = 987 we have 2 situations as below
-            if ((h1 + h2) != 99) { 
+    switch (d) {
+        case 987: {
+            if (h1 + h2 != 99) {
                 if (hp1 != 999) p = 0.00l;
             }
+            break;
         }
-    } 
-    else { // hp2 = 888 
-        switch (hp1) { // At this point writing if statement here would be the pain in the ass
-            case 777 : break;
-            case 888 : break;
-            case 900 : break;
-            case 999 : {
-                p = 1.00l;
-                break;
+        default: {
+            switch (hp2) {
+                case 888: {
+                    switch(hp1) {
+                        case 777 : break;
+                        case 888 : break;
+                        case 900 : break;
+                        case 999 : {
+                        //     if (d == 987) {
+                        //     break;
+                        // }
+                            p = 1.00l;
+                            break;
+                        }
+                        default : {
+                            p = 0.01l;
+                            break;
+                        }
+                    }
+                    break;                  
+                }
+                default : {
+                    switch (hp1) {
+                        case 777: {
+                                if (p1 < p2 || h1 < h2) {
+                                    d = 1;
+                                    p1 = (double)hp1 * (double)(1000.00l - d) / (double)1000.00;
+                                    p2 = (double)hp2 * (double)d / (double)1000.00;
+                                    p = ((double)h1 * p1 - (double)h2 * p2) / ((double)h1 * p1 + (double)h2 * p2);
+                            }
+                            break;
+                        }
+
+                        case 888 : { 
+                            h1 *= 10; // Don't change h2 accordingly
+                            p = ((double)h1 * p1 - (double)h2 * p2) / ((double)h1 * p1 + (double)h2 * p2);
+                            break;
+                        }
+
+                        case 900 : {
+                            if  (p < 0.50l) p = 0.50l;
+                            break;    
+                        }   
+                    }
+
+                    if (d != 987) { // Since with d = 987 and with special pairs of hp1 and hp2 below, it will end up a different result
+                        if ((hp1 == 220 && hp2 == 284) || (hp1 == 284 && hp2 == 220)) p = 0.50l; 
+                    }
+                    else { // in the case of d = 987 we have 2 situations as below
+                        if ((h1 + h2) != 99) { 
+                            if (hp1 != 999) p = 0.00l;
+                        }
+                    }
+                    break;   
+                } 
             }
-            default : {
-                p = 0.01l;
-                break;
-            }
-        }      
+        }
     }
+
+<<<<<<< HEAD
 
     /* process the output */
     cout << fixed << setprecision(2) << p;
+
+
+=======
+    /* process the output */
+    cout << fixed << setprecision(2) << p;
+>>>>>>> 2eacd9b8fa45cc630559194660ed0bfb99cb7e97
 }
 
 int readFile(string filename, int& hp1, int& hp2, int& d) {
-    ifstream myfile(filename);
-    if (myfile.is_open()) {
-        myfile >> hp1 >> hp2 >> d;
+    // ifstream myfile(filename);
+    // if (myfile.is_open()) {
+    //     myfile >> hp1 >> hp2 >> d;
 
-        return 1;
-    }
-    else return 0;
+    //     return 1;
+    // }
+    // else return 0;
+    return 0;
 }
 
 int main(int argc, char** argv) {
-    if (argc == 2) {
-        int hp1 = 0;
-        int hp2 = 0;
-        int d = 0;
+    // if (argc == 2) {
+    //     int hp1 = 0;
+    //     int hp2 = 0;
+    //     int d = 0;
 
-        int readInput = readFile(argv[1], hp1, hp2, d);
+    //     int readInput = readFile(argv[1], hp1, hp2, d);
         
-        if (readInput) combat(hp1, hp2, d);
-        else cout << "Cannot read input file";
-    }
-    else cout << "Incorrect arguments format";
+    //     if (readInput) combat(hp1, hp2, d);
+    //     else cout << "Cannot read input file";
+    // }
+    // else cout << "Incorrect arguments format";
+    combat(777,188, 500);
+    return 0;
 }
